8a40d76afa4b80bbc709f450ad60b1a19650ef9e
Bassel Haidar <bass.haidar@gmail.com>
2019-05-14 16:30:06 -0400

    Lab Done

--- school.py
+++ school.py
@@ -1,1 +1,22 @@
-class School:
+class School():
+
+    def __init__(self, name):
+        self.name = name
+        self.roster = {}
+
+    def add_student(self, fullname, grade):
+        self.fullname = fullname
+        self.grade = grade
+        if grade in self.roster:
+            self.roster[grade].append(fullname)
+        else:
+            self.roster[grade] = [fullname]
+
+    def grade_1(self, grade):
+        return self.roster[grade]
+
+    def sort_roster(self):
+        sort_dict = self.roster
+        for key in sort_dict:
+            sort_dict[key].sort()
+        return sort_dict
